7c9adfe9a9447a35ecf0e94463d314e111402db7
Neetesh Kumar <kumarneetesh24@gmail.com>
2016-11-09 02:00:57 +0530

    initial commit

---
+++ applet.py
@@ -0,0 +1,17 @@
+import gi
+gi.require_version('Gtk','3.0')
+gi.require_version('AppIndicator3','0.1')
+
+from gi.repository import Gtk as gtk
+from gi.repository import AppIndicator3 as appindicator
+
+APPINDICATOR_ID = 'network-proxy-toggle'
+
+def main():
+    indicator = appindicator.Indicator.new(APPINDICATOR_ID,'noimage',appindicator.IndicatorCategory.SYSTEM_SERVICES)
+    indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+    indicator.set_menu(gtk.Menu())
+    gtk.main()
+
+if __name__ == "__main__":
+    main()
