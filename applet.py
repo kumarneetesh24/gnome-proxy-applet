f6179aa33b65427341c125efeaed3617af3a1721
Neetesh Kumar <kumarneetesh24@gmail.com>
2016-11-09 09:16:22 +0530

    quit menuItem added

--- applet.py
+++ applet.py
@@ -1,3 +1,4 @@
+import signal
 import gi
 gi.require_version('Gtk','3.0')
 gi.require_version('AppIndicator3','0.1')
@@ -10,8 +11,19 @@ APPINDICATOR_ID = 'network-proxy-toggle'
 def main():
     indicator = appindicator.Indicator.new(APPINDICATOR_ID,'noimage',appindicator.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
-    indicator.set_menu(gtk.Menu())
+    indicator.set_menu(build_menu())
     gtk.main()
+
+def build_menu():
+    menu = gtk.Menu()
+    item_quit = gtk.MenuItem('quit')
+    item_quit.connect('activate', quit)
+    menu.append(item_quit)
+    menu.show_all()
+    return menu
+
+def quit(source):
+    gtk.main_quit()
 
 if __name__ == "__main__":
     main()
