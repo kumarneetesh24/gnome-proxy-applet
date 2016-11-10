d2ea78c6ffb3a96e36e748a3aca3cfe7f675b9e3
Neetesh Kumar <kumarneetesh24@gmail.com>
2016-11-11 00:26:10 +0530

    startup script changed

--- install.sh
+++ install.sh
@@ -5,41 +5,42 @@ die() {
     exit 1
 }
 
-if [ "$(id -u)" -ne 0 ] ; then
-    echo "You must run this script as root. Sorry!"
-    exit 
-fi
+#if [ "$(id -u)" -ne 0 ] ; then
+#    echo "You must run this script as root. Sorry!"
+#    exit 1
+#fi
 
-APPLET_FILE_DEST="/usr/local/bin/proxy-applet"  
-INIT_FILE="/etc/init.d/proxy-applet"
-TMP_FILE="temp"
+APPLET_FILE_DEST="/usr/local/bin/proxy-applet"
+TMP_FILE="proxy-applet.desktop"
 
 #copying applet program
-cp applet.py $APPLET_FILE_DEST || die "could not copy applet to bin"
-chmod a+x $APPLET_FILE_DEST
+sudo cp applet.py $APPLET_FILE_DEST || die "could not copy applet to bin"
+sudo chmod a+x $APPLET_FILE_DEST
 
 #copying icon
-cp proxy-applet.svg /usr/share/icons/proxy-applet.svg || die "could not copy image"
+sudo cp proxy-applet.svg /usr/share/icons/proxy-applet.svg || die "could not copy image"
 
 cat > $TMP_FILE <<EOT
-#!/bin/sh
+[Desktop Entry]
+Type=Application
+Exec=/usr/local/bin/proxy-applet
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name[en_IN]=proxy-applet
+Name=proxy-applet
+Comment[en_IN]=change proxy from the top menu
+Comment=change proxy from the top menu
 
-### BEGIN INIT INFO
-# Provides:          proxy-applet 
-# Required-Start:    udev
-# Required-Stop:
-# Should-Start:
-# Default-Start:     2 3 4 5
-# Default-Stop:
-# Short-Description: indicator to change proxy between none and manual
-### END INIT INFO
 EOT
 
-cp $TMP_FILE $INIT_FILE && \
-   chmod +x $INIT_FILE || die "could not copy applet init file to $INIT_FILE"
+mkdir -p ~/.config/autostart || die " could not create autostart directory"
+
+cp $TMP_FILE ~/.config/autostart/proxy-applet.desktop || die "could not copy applet init file to autostart"
 rm $TMP_FILE 
 
-update-rc.d proxy-applet defaults && echo "success" 
-
+nohup proxy-applet &> /dev/null &
+echo "success"
 
 exit 0
+
