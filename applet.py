fb9b1d542e54852e3f3e7fa8fd3cc4aa23e478d0
Neetesh Kumar <kumarneetesh24@gmail.com>
2016-11-11 01:32:27 +0530

    toogle option and notifier removed

--- applet.py
+++ applet.py
@@ -5,12 +5,12 @@ import gi
 
 gi.require_version('Gtk','3.0')
 gi.require_version('AppIndicator3','0.1')
-gi.require_version('Notify','0.7')
+#gi.require_version('Notify','0.7')
 
 
 from gi.repository import Gtk as gtk
 from gi.repository import AppIndicator3 as appindicator
-from gi.repository import Notify as notify
+#from gi.repository import Notify as notify
 
 APPINDICATOR_ID = 'network-proxy-toggle'
 mode = "org.gnome.system.proxy mode"
@@ -22,7 +22,7 @@ def main():
     indicator = appindicator.Indicator.new(APPINDICATOR_ID,img_path,appindicator.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
     indicator.set_menu(build_menu())
-    notify.init(APPINDICATOR_ID)
+#    notify.init(APPINDICATOR_ID)
     gtk.main()
 
 def checkProxy(): 
@@ -36,14 +36,14 @@ def checkProxy():
 def build_menu():
     menu = gtk.Menu()
     
-    item_toggle = gtk.MenuItem('toggle')
-    item_toggle.connect('activate',toggle)
-    menu.append(item_toggle)
+#    item_toggle = gtk.MenuItem('toggle')
+#    item_toggle.connect('activate',toggle)
+#    menu.append(item_toggle)
     
     # Separator
-    smi = gtk.SeparatorMenuItem()
-    smi.show()
-    menu.append(smi)
+#    smi = gtk.SeparatorMenuItem()
+#    smi.show()
+#    menu.append(smi)
     
     #group for radio icon
     grp = []
@@ -80,16 +80,16 @@ def manual(source):
     os.popen("gsettings set "+mode+" manual")
 
 
-def toggle(source):
-    data = os.popen("gsettings get "+mode).read()
-    if(data == "'none'\n"):
-        os.popen("gsettings set "+mode+" manual").read()
-        host_data = os.popen("gsettings get "+host).read()
-        port_data = os.popen("gsettings get "+port).read()
-        notify_for_manual(host_data,port_data)
-    else:
-        os.popen("gsettings set "+mode+" none").read()
-        notify.Notification.new("Proxy: none",None).show()
+#def toggle(source):
+#    data = os.popen("gsettings get "+mode).read()
+#    if(data == "'none'\n"):
+#        os.popen("gsettings set "+mode+" manual").read()
+#        host_data = os.popen("gsettings get "+host).read()
+#        port_data = os.popen("gsettings get "+port).read()
+#        notify_for_manual(host_data,port_data)
+#    else:
+#        os.popen("gsettings set "+mode+" none").read()
+#        notify.Notification.new("Proxy: none",None).show()
     
 
 def notify_for_manual(host_data,port_data):
@@ -97,7 +97,7 @@ def notify_for_manual(host_data,port_data):
     notify.Notification.new("Proxy "+data,None).show()
 
 def quit(source):
-    notify.uninit()
+#    notify.uninit()
     gtk.main_quit()
 
 if __name__ == "__main__":
